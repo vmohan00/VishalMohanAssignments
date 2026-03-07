9550a9b330ccfae8e7f2824dc194e9c62095cbdf
vmohan00 <vishalmoha@gmail.com>
2026-03-07 06:51:29 -0500

    Add files via upload
    Day 1

---
+++ Day_1/Day_1_Practice_primitive_data_types.js
@@ -0,0 +1,12 @@
+var firstName = "Vishal";
+var mobileNumber = 6476364836;
+var isAutomation = false;
+var hasPlaywright; 
+
+console.log("Value: " + firstName);
+console.log("Value: " + mobileNumber);
+console.log("Value: " + isAutomation);
+console.log("Value: " + hasPlaywright);
+
+
+
